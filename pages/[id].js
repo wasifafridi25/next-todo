9966f543882f8fae274b765f8bbee64ddc53ef95
Bhuiyan Shadman Wasif <waseefafridi39@gmail.com>
2023-12-30 20:05:54 -0800

    Used getServerSideProps and context.query.id to get the id from the url

--- pages/[id].js
+++ pages/[id].js
@@ -1,29 +1,55 @@
-
-import { useRouter } from 'next/router'
 import { getDoc, doc} from "firebase/firestore"
 import { db } from '@/firebase';
 import { useState } from 'react';
 
-export default function Todo() {
-    const [todo, setTodo] = useState({})
+export const getServerSideProps =async (context) => {
+    // console.log(context)
+    const id = context.query.id;
+    // console.log(id)
+    const docSnap = await getDoc(doc(db, "todos", id))
+    const data = docSnap.data()
 
-    const router = useRouter()
-    const id = router.query.id;
-    // const {id} = router.query;
-
-    const getData = async() => {
-        const docSnap = await getDoc(doc(db, "todos", id));
-        const data = docSnap.data()
-        setTodo(data)
+    return {
+        props: {
+            data
+        }
     }
-    getData()
+}
 
-
-
+export default function Todo({ data }) {
+  
   return (
     <div className="container">
-      <h1>Todo title: {todo.title}</h1>
-      <h2>Todo detail: {todo.detail}</h2>
+      <h1>Todo title: {data.title}</h1>
+      <h2>Todo detail: {data.detail}</h2>
     </div>
   )
 }
+// import { useRouter } from 'next/router'
+// import { getDoc, doc} from "firebase/firestore"
+// import { db } from '@/firebase';
+// import { useState } from 'react';
+
+// export default function Todo() {
+//     const [todo, setTodo] = useState({})
+
+//     const router = useRouter()
+//     const id = router.query.id;
+//     // const {id} = router.query;
+
+//     const getData = async() => {
+//         const docSnap = await getDoc(doc(db, "todos", id));
+//         const data = docSnap.data()
+//         setTodo(data)
+//     }
+//     getData()
+
+
+
+//   return (
+//     <div className="container">
+//       <h1>Todo title: {todo.title}</h1>
+//       <h2>Todo detail: {todo.detail}</h2>
+//     </div>
+//   )
+// }
